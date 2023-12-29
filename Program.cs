3475643699e93f44084b8065fdcc9c583e58e78d
mrpis2 <warhammerprogamer@gmail.com>
2023-12-29 21:57:25 +0700

    count

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] s = { "a", "b", "c", "a", "d", "e" };
+        Console.WriteLine("Nhap ki tu can dem: ");
+        string x = Console.ReadLine();
+        int count = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (x == s[i])
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("so luong ki tu la: " + count);
+    }
+}
